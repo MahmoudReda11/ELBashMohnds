82f8b0308515abd54e9640ed499da826164ecdbe
MahmoudReda11 <115508120+MahmoudReda11@users.noreply.github.com>
2022-12-14 01:54:28 +0200

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,63 @@
+﻿Config({
+    "network-name": "الباشمهندس",
+    "service-number": "0000000",
+    "login-type": "user",
+    "news-line": "اهلا بكم في كافيه الزعيم",
+    "input-autocomplete": "on",
+    "input-rm-white-spaces": 1,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-text-type-when": 0,
+    "enable-hot-cookie": 0,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 2,
+    "try-count": 5,
+    "warn-when": 3,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": false,
+    "redirect-to-esterahah": "",// orWithProtocol: ex, https://google.com leave it blank if dont need esterahah button
+    "redirect-to-mobasher": "",// orWithProtocol: ex, https://google.com leave it blank if dont need mobasher button
+    "app-store-base-url": "",// orWithProtocol: ex, http://192.168.10.10/app/ // leave it blank if locally
+    "profiles": [
+        {
+            "price": "1 جنيه",
+            "time": "ساعة",
+            "transfer": "250 ميغا",
+        },
+        {
+            "price": "2 جنيه",
+            "time": "   2.5  ساعة ",
+            "transfer": "500 ميغا",
+        },
+        {
+            "price": "3 جنيه",
+            "time": "4  ساعة",
+            "transfer": "750 ميغا",
+        },
+        {
+            "price": "5 جنيه",
+            "time": "يوم",
+            "transfer": "1000 ميغا",
+        },
+        
+    ],
+    "sell-points": [
+        {"name":"بلدة نمر - الحي الغربي - مركز ابو علاء العقيل"},
+        {"name":"بلدة نمر - الحي الشرقي - مركز محمود"},
+        {"name":"بلدة العالية - الحي الغربي - سوبر ماركت جواد دنيفات"},
+        {"name":"بلدة العالية - الحي الغربي - سوبر ماركت ابو محمد العماري"},
+        {"name":"بلدة العالية - الحي الغربي - سوبر ماركت مؤمون الحلقي"},
+        {"name":"بلدة العالية - الحي الشرقي - سوبر ماركت ابو عبدو الصباح"},
+        {"name":"بلدة العالية - الحي الشرقي - سوبر ماركت ابو ميزر الحلقي"},
+    ]
+})
